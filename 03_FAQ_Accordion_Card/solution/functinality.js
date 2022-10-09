e53cb1c9dd684676495a0c8b625bb57f99661171
CVakratsas <c.vakratsas@gmail.com>
2022-10-09 18:54:42 +0300

    A few things missing

--- 03_FAQ_Accordion_Card/solution/functinality.js
+++ 03_FAQ_Accordion_Card/solution/functinality.js
@@ -1,10 +1,39 @@
-function clicked(ans) {
-  var radios = document.getElementsByName("questions");
+"use strict";
 
-  for (var i = 0; i < radios.length; i++) {
-    if (radios[i].checked) {
-      var answer = document.getElementById(ans);
-      break;
-    }
-  }
+function toggleClass(p, label, arrow) {
+  var labels = document.querySelectorAll(".questions");
+
+  // for (var i = 0; i < labels.length; i++) {
+  //   var current = document.getElementById("l" + i);
+  //   // console.log(labels[i].classList.contains("makeBold"));
+  //   // if (labels[i].classList.contains("makeBold")) console.log(" hello there");
+  //   if (labels[i].classList.contains("makeBold") && labels[i] != current) {
+  //     var str = "a" + i;
+  //     console.log(i);
+  //     // document.getElementById(str).classList.toggle("hide");
+  //   }
+  // }
+
+  var p = document.getElementById(p);
+  p.classList.toggle("hide");
+  var label = document.getElementById(label);
+  label.classList.toggle("makeBold");
+  var arrow = document.getElementById(arrow);
+  arrow.classList.toggle("rotate");
+
+  // labels.forEach((e) => {
+  //   if (e !== label) {
+  //     e.classList.toggle("hide");
+  //   }
+  // });
+}
+
+function show(ans) {
+  var answer = document.getElementById(ans);
+  answer.style.display = "block";
+}
+
+function hide(ans) {
+  var answer = document.getElementById(ans);
+  answer.style.display = "none";
 }
